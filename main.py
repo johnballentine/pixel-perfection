9c0dd72ee89a3a3c6553a8dbcc78f82a80fd48c2
John Ballentine <johnballentine10@gmail.com>
2023-09-04 19:35:02 -0700

    Added perlin noise backgrounds

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import cv2
 import os
 import numpy as np
 import random
+from perlin_noise import PerlinNoise
 
 def rewrite_filename_with_string(filename, extra_string):
     # Split the filename into base and extension
@@ -46,6 +47,41 @@ def increase_canvas(image, new_dimensions, max_random_offset=0):
 
     return enlarged_image
 
+def generate_perlin_noise_image(size=(256, 256), scale=100, octaves=1):
+    perlin = PerlinNoise(octaves=octaves)
+    perlin_image = np.zeros(size, dtype=np.uint8)
+    
+    for y in range(size[0]):
+        for x in range(size[1]):
+            value = perlin([x / scale, y / scale])
+            normalized_value = (value + 1) * 0.5  # Normalize to [0, 1]
+            perlin_image[y, x] = int(normalized_value * 255)
+    
+    color1 = random_color()
+    color2 = random_color()
+    
+    blended_image = np.zeros((*size, 3), dtype=np.uint8)
+    
+    for y in range(size[0]):
+        for x in range(size[1]):
+            weight = perlin_image[y, x]
+            blended_color = color1 * weight + color2 * (1 - weight)
+            blended_image[y, x] = blended_color
+
+    random_sigma = np.random.uniform(7.0, 50.0)
+    blurred_image = cv2.GaussianBlur(blended_image, ksize = (0,0), sigmaX = random_sigma, sigmaY = random_sigma)
+    
+    # Slightly desaturate the image by subtracting a small value from each channel
+    desaturation_value = 20  # Adjust this value for the desired desaturation effect
+    desaturated_image = blurred_image - desaturation_value
+    
+    # Ensure pixel values are within [0, 255]
+    desaturated_image = np.clip(desaturated_image, 0, 255).astype(np.uint8)
+    
+    return desaturated_image
+
+
+
 def rotate_image(image, angle):
     ''' Rotate an image "angle" degrees.
         "image" is the image to rotate.
@@ -65,13 +101,15 @@ def rotate_image(image, angle):
     return rotated
 
 
-def add_background(image, bgcolor, angle=2):
-    ''' Overlays an image with a transparent background on top of a background color.
+def add_background(image, angle=2):
+    ''' Overlays an image with a transparent background on top of a Perlin noise background.
         "image" is an OpenCV image with a transparency alpha channel
-        "bgcolor" is a BGR color
         "angle" is the degree to which the image should be rotated
         
         returns a BGR OpenCV image with no alpha channel'''
+    
+    # Generate Perlin noise background image
+    perlin_noise = generate_perlin_noise_image()
     
     # First, rotate the image if angle is not 0
     if angle != 0:
@@ -80,20 +118,21 @@ def add_background(image, bgcolor, angle=2):
     # Split the image into BGRA channels
     B, G, R, A = cv2.split(image)
 
-    # Create a 3-channel color image (RGB)
-    fill_color_img = np.zeros([image.shape[0], image.shape[1], 3]).astype('uint8')
-    fill_color_img[:, :] = bgcolor
+    # Convert Perlin noise to 3-channel BGR image
+    perlin_rgb = cv2.cvtColor(perlin_noise, cv2.COLOR_BGRA2BGR)
 
     # Calculate the weighting of each pixel based on transparency
     A = A / 255.0
-    fill_weight = 1 - A
+    perlin_weight = 1 - A
     image_weight = A
 
-    # Split fill color into channels
-    fill_B, fill_G, fill_R = cv2.split(fill_color_img)
-    blend_B = (B * image_weight + fill_B * fill_weight).astype(np.uint8)
-    blend_G = (G * image_weight + fill_G * fill_weight).astype(np.uint8)
-    blend_R = (R * image_weight + fill_R * fill_weight).astype(np.uint8)
+    # Split Perlin noise color channels
+    perlin_B, perlin_G, perlin_R = cv2.split(perlin_rgb)
+    
+    # Calculate the blended color channels
+    blend_B = (B * image_weight + perlin_B * perlin_weight).astype(np.uint8)
+    blend_G = (G * image_weight + perlin_G * perlin_weight).astype(np.uint8)
+    blend_R = (R * image_weight + perlin_R * perlin_weight).astype(np.uint8)
 
     # Merge channels into final image but only keep BGR channels
     return cv2.merge([blend_B, blend_G, blend_R])
@@ -245,7 +284,7 @@ def generate(image,
     bicubic_upscaled = upscale_bicubic(padded, (256,256))
     bicubic_blur = gaussian_blur(bicubic_upscaled)
     bicubic_vividness = alter_vividness(bicubic_blur)
-    bicubic_background = add_background(bicubic_vividness, random_color())
+    bicubic_background = add_background(bicubic_vividness)
     bicubic_warp = warp(bicubic_background)
     bicubic_jpeg = add_jpeg_artifacts(bicubic_warp, jpeg_quality)
 
@@ -260,20 +299,29 @@ if __name__ == "__main__":
     input_directory = "data"
     output_directory = "./output"
     files_in_directory = os.listdir(input_directory)
+    files_in_directory.sort()  # Sort the files alphabetically
 
     image_size = (256, 256)
 
     # Create the output directory if it doesn't exist
     os.makedirs(output_directory, exist_ok=True)
+    
+    num_files = len(files_in_directory)
+    file_counter = 0
 
     for filename in files_in_directory:
         if filename.lower().endswith(('.png', '.jpg', '.jpeg')):
+            file_counter += 1
+            
             # Specify the input image path
             input_image_path = os.path.join(input_directory, filename)
 
             # Read the input image
             input_image = cv2.imread(input_image_path, cv2.IMREAD_UNCHANGED)
-            #input_image_upscaled = upscale_nearest(input_image, 16)
+            # input_image_upscaled = upscale_nearest(input_image, 16)
+
+            # Print the filename of the input image
+            print(f"Processing input image {file_counter}/{num_files}: {filename}")
 
             # Write the original image with increased canvas size to the output directory with filename modified to include "_label"
             label_filename = rewrite_filename_with_string(filename, "label")
@@ -285,3 +333,6 @@ if __name__ == "__main__":
             # Write the output image to the output directory with filename modified to include "_input"
             processed_filename = rewrite_filename_with_string(filename, "input")
             cv2.imwrite(os.path.join(output_directory, processed_filename), output_image)
+
+            # Print the output filename after processing
+            print(f"Saved: {processed_filename}")
